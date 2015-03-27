3b09ce4daf7091e655b0aa6a57c99895e92fe141
Yongjia Liu <noirozr@gmail.com>
2015-03-27 20:49:11 +0800

    swift code
    add swift code

---
+++ nipponColor.swift
@@ -0,0 +1,267 @@
+//
+//  nipponColor.swift
+//  Surf
+//
+//  Created by Noirozr on 15/3/26.
+//  Copyright (c) 2015年 Yongjia Liu. All rights reserved.
+//
+
+import UIKit
+
+//http://nipponcolors.com
+
+extension UIColor {
+    
+    class func MOMOColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 245.0/255.0, green: 150.0/255.0, blue: 170.0/255.0, alpha: alpha)
+    }
+    
+    class func KUWAZOMEColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 100.0/255.0, green: 54.0/255.0, blue: 60.0/255.0, alpha: alpha)
+    }
+    
+    class func IKKONZOMEColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 244.0/255.0, green: 167.0/255.0, blue: 185.0/255.0, alpha: alpha)
+    }
+    
+    class func TAIKOHColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 248.0/255.0, green: 195.0/255.0, blue: 205.0/255.0, alpha: alpha)
+    }
+    
+    class func SUOHColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 142.0/255.0, green: 53.0/255.0, blue: 74.0/255.0, alpha: alpha)
+    }
+    
+    class func KOHBAIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 225.0/255.0, green: 107.0/255.0, blue: 140.0/255.0, alpha: alpha)
+    }
+   
+    class func NADESHIKOColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 220.0/255.0, green: 159.0/255.0, blue: 180.0/255.0, alpha: alpha)
+    }
+    
+    class func KARAKURENAIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 208.0/255.0, green: 16.0/255.0, blue: 76.0/255.0, alpha: alpha)
+    }
+    
+    class func UMENEZUMIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 158.0/255.0, green: 122.0/255.0, blue: 122.0/255.0, alpha: alpha)
+    }
+    
+    class func SAKURAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 254.0/255.0, green: 223.0/255.0, blue: 225.0/255.0, alpha: alpha)
+    }
+    
+    class func NAKABENIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 219.0/255.0, green: 77.0/255.0, blue: 109.0/255.0, alpha: alpha)
+    }
+    
+    class func IMAYOHColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 208.0/255.0, green: 90.0/255.0, blue: 110.0/255.0, alpha: alpha)
+    }
+    
+    class func USUBENIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 232.0/255.0, green: 122.0/255.0, blue: 144.0/255.0, alpha: alpha)
+    }
+    
+    class func ICHIGOColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 181.0/255.0, green: 73.0/255.0, blue: 91.0/255.0, alpha: alpha)
+    }
+    
+    class func JINZAMOMIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 235.0/255.0, green: 122.0/255.0, blue: 119.0/255.0, alpha: alpha)
+    }
+    
+    class func SAKURANEZUMIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 177.0/255.0, green: 150.0/255.0, blue: 147.0/255.0, alpha: alpha)
+    }
+    
+    class func KOKIAKEColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 134.0/255.0, green: 71.0/255.0, blue: 63.0/255.0, alpha: alpha)
+    }
+    
+    class func CYOHSYUNColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 191.0/255.0, green: 103.0/255.0, blue: 102.0/255.0, alpha: alpha)
+    }
+    
+    class func TOKIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 238.0/255.0, green: 169.0/255.0, blue: 169.0/255.0, alpha: alpha)
+    }
+    
+    class func KURENAIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 203.0/255.0, green: 27.0/255.0, blue: 69.0/255.0, alpha: alpha)
+    }
+    
+    class func ENJIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 159.0/255.0, green: 53.0/255.0, blue: 58.0/255.0, alpha: alpha)
+    }
+    
+    class func EBICHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 115.0/255.0, green: 67.0/255.0, blue: 56.0/255.0, alpha: alpha)
+    }
+    
+    class func KURIUMEColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 144.0/255.0, green: 72.0/255.0, blue: 64.0/255.0, alpha: alpha)
+    }
+    
+    class func HAIZAKURAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 215.0/255.0, green: 196.0/255.0, blue: 187.0/255.0, alpha: alpha)
+    }
+    
+    class func SHINSYUColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 171.0/255.0, green: 59.0/255.0, blue: 58.0/255.0, alpha: alpha)
+    }
+    
+    class func AKABENIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 203.0/255.0, green: 64.0/255.0, blue: 66.0/255.0, alpha: alpha)
+    }
+    
+    class func SUOHKOHColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 169.0/255.0, green: 99.0/255.0, blue: 96.0/255.0, alpha: alpha)
+    }
+    
+    class func AZUKIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 149.0/255.0, green: 74.0/255.0, blue: 69.0/255.0, alpha: alpha)
+    }
+    
+    class func SANGOSYUColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 241.0/255.0, green: 124.0/255.0, blue: 103.0/255.0, alpha: alpha)
+    }
+    
+    class func MIZUGAKIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 185.0/255.0, green: 136.0/255.0, blue: 125.0/255.0, alpha: alpha)
+    }
+    
+    class func BENIKABAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 181.0/255.0, green: 68.0/255.0, blue: 52.0/255.0, alpha: alpha)
+    }
+    
+    class func AKEBONOColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 241.0/255.0, green: 148.0/255.0, blue: 131.0/255.0, alpha: alpha)
+    }
+    
+    class func BENITOBIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 153.0/255.0, green: 70.0/255.0, blue: 57.0/255.0, alpha: alpha)
+    }
+    
+    class func KUROTOBIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 85.0/255.0, green: 66.0/255.0, blue: 54.0/255.0, alpha: alpha)
+    }
+    
+    class func GINSYUColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 199.0/255.0, green: 62.0/255.0, blue: 58.0/255.0, alpha: alpha)
+    }
+    
+    class func AKEColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 204.0/255.0, green: 84.0/255.0, blue: 58.0/255.0, alpha: alpha)
+    }
+    
+    class func KAKISHIBUColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 163.0/255.0, green: 94.0/255.0, blue: 71.0/255.0, alpha: alpha)
+    }
+    
+    class func HIWADAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 133.0/255.0, green: 72.0/255.0, blue: 54.0/255.0, alpha: alpha)
+    }
+    
+    class func SHIKANCHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 181.0/255.0, green: 93.0/255.0, blue: 76.0/255.0, alpha: alpha)
+    }
+    
+    class func ENTANColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 215.0/255.0, green: 84.0/255.0, blue: 85.0/255.0, alpha: alpha)
+    }
+    
+    class func SYOJYOHIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 232.0/255.0, green: 48.0/255.0, blue: 21.0/255.0, alpha: alpha)
+    }
+    
+    class func BENIHIWADAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 136.0/255.0, green: 76.0/255.0, blue: 58.0/255.0, alpha: alpha)
+    }
+    
+    class func ARAISYUColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 251.0/255.0, green: 150.0/255.0, blue: 110.0/255.0, alpha: alpha)
+    }
+    
+    class func EDOCHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 175.0/255.0, green: 95.0/255.0, blue: 60.0/255.0, alpha: alpha)
+    }
+    
+    class func TERIGAKIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 196.0/255.0, green: 98.0/255.0, blue: 67.0/255.0, alpha: alpha)
+    }
+    
+    class func BENGARAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 154.0/255.0, green: 80.0/255.0, blue: 52.0/255.0, alpha: alpha)
+    }
+    
+    class func KURIKAWACHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 106.0/255.0, green: 64.0/255.0, blue: 40.0/255.0, alpha: alpha)
+    }
+    
+    class func BENIHIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 247.0/255.0, green: 92.0/255.0, blue: 47.0/255.0, alpha: alpha)
+    }
+    
+    class func TOBIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 114.0/255.0, green: 72.0/255.0, blue: 50.0/255.0, alpha: alpha)
+    }
+    
+    class func KABACHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 179.0/255.0, green: 92.0/255.0, blue: 55.0/255.0, alpha: alpha)
+    }
+    
+    class func ENSYUCHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 202.0/255.0, green: 120.0/255.0, blue: 83.0/255.0, alpha: alpha)
+    }
+    
+    class func SOHIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 237.0/255.0, green: 120.0/255.0, blue: 74.0/255.0, alpha: alpha)
+    }
+    
+    class func OHNIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 240.0/255.0, green: 94.0/255.0, blue: 28.0/255.0, alpha: alpha)
+    }
+    
+    class func TOKIGARACHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 219.0/255.0, green: 142.0/255.0, blue: 113.0/255.0, alpha: alpha)
+    }
+    
+    class func KARACHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 180.0/255.0, green: 113.0/255.0, blue: 87.0/255.0, alpha: alpha)
+    }
+    
+    class func MOMOSHIOCHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 114.0/255.0, green: 73.0/255.0, blue: 56.0/255.0, alpha: alpha)
+    }
+    
+    class func KOKIKUCHINASHIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 251.0/255.0, green: 153.0/255.0, blue: 102.0/255.0, alpha: alpha)
+    }
+    
+    class func KABAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 193.0/255.0, green: 105.0/255.0, blue: 60.0/255.0, alpha: alpha)
+    }
+    
+    class func SODENKARACHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 160.0/255.0, green: 103.0/255.0, blue: 75.0/255.0, alpha: alpha)
+    }
+    
+    class func SHISHIColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 240.0/255.0, green: 169.0/255.0, blue: 134.0/255.0, alpha: alpha)
+    }
+    
+    class func SUZUMECHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 143.0/255.0, green: 90.0/255.0, blue: 60.0/255.0, alpha: alpha)
+    }
+    
+    class func AKAKOHColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 227.0/255.0, green: 145.0/255.0, blue: 110.0/255.0, alpha: alpha)
+    }
+    
+    class func KOGECHAColor(alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: 86.0/255.0, green: 68.0/255.0, blue: 46.0/255.0, alpha: alpha)
+    }
+    
+}
